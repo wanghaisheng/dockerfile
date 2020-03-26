c08d681eeef5ad0682c15aa487ab78c6cf8be618
Tarocch1 <huiben.fang@gmail.com>
2020-03-26 15:00:50 +0800

    修正调试信息错误

--- .github/actions/v2ray/push/index.js
+++ .github/actions/v2ray/push/index.js
@@ -10,7 +10,7 @@ const version = require('../../../../v2ray/version.json');
   };
   console.log(`Login to docker hub with user '${username}'.`);
   await exec.exec('docker', ['login', '-u', username, '-p', password], options);
-  console.log(`Build v2ray image with tag '${version.version}'.`);
+  console.log(`Push v2ray image with tag '${version.version}'.`);
   await exec.exec('docker', ['push', `tarocch1/v2ray:${version.version}`], options);
   console.log("Push v2ray image with tag 'latest'.");
   await exec.exec('docker', ['push', 'tarocch1/v2ray:latest'], options);
