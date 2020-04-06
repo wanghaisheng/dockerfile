49fbf9118de4b67aa71562a3448bd5da5134875d
Tarocch1 <huiben.fang@gmail.com>
2020-04-06 16:45:18 +0800

    添加 mirai

---
+++ .github/actions/mirai/build/index.js
@@ -0,0 +1,9 @@
+const exec = require('@actions/exec');
+
+(async function () {
+  const options = {
+    cwd: './mirai',
+  };
+  console.log("Build mirai image with tag 'latest'.");
+  await exec.exec('docker', ['build', '-t', 'tarocch1/mirai:latest', '.'], options);
+})();
