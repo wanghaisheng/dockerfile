3f9d7bbddd68c4a84e0aa6b092ffb67015b2b332
Tarocch1 <huiben.fang@gmail.com>
2020-07-08 19:06:16 +0800

    修改docker构建方式

--- .github/actions/v2ray/build/index.js
+++ .github/actions/v2ray/build/index.js
@@ -7,6 +7,6 @@ const version = require('../../../../v2ray/version.json');
   };
   console.log(`Build v2ray image with tag '${version.version}'.`);
   await exec.exec('docker', ['build', '-t', `tarocch1/v2ray:${version.version}`, '.'], options);
-  console.log("Build v2ray image with tag 'latest'.");
-  await exec.exec('docker', ['build', '-t', 'tarocch1/v2ray:latest', '.'], options);
+  console.log("Create tag 'latest'.");
+  await exec.exec('docker', ['tag', `tarocch1/v2ray:${version.version}`, 'tarocch1/v2ray:latest'], options);
 })();
