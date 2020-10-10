e731fa004f8516ca2bdcf081a65bd00e4744dba9
Tarocch1 <huiben.fang@gmail.com>
2020-10-10 11:44:37 +0800

    v2ray 支持多架构

---
+++ v2ray/build.sh
@@ -0,0 +1,11 @@
+cd ./v2ray
+
+read VERSION < ./version
+
+docker buildx build \
+  --platform linux/amd64,linux/386,linux/ppc64le,linux/s390x,linux/arm64,linux/arm/v7,linux/arm/v6 \
+  -t tarocch1/v2ray:latest \
+  -t tarocch1/v2ray:${VERSION} \
+  -t registry.cn-hongkong.aliyuncs.com/tarocch1/v2ray:latest \
+  --push \
+  .
