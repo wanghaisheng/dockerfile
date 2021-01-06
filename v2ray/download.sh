c6ebb9eeb0ad05632e0a119f0b1c9d52724abb3b
Tarocch1 <huiben.fang@gmail.com>
2021-01-06 09:54:02 +0800

    v2ray 去掉 s390x 与 ppc64le，添加 riscv64

--- v2ray/download.sh
+++ v2ray/download.sh
@@ -3,6 +3,7 @@ read VERSION < ./version
 case ${TARGETPLATFORM} in
   linux/amd64) FILENAME="v2ray-linux-64.zip";;
   linux/386) FILENAME="v2ray-linux-32.zip";;
+  linux/riscv64) FILENAME="v2ray-linux-riscv64.zip";;
   linux/ppc64le) FILENAME="v2ray-linux-ppc64le.zip";;
   linux/s390x) FILENAME="v2ray-linux-s390x.zip";;
   linux/arm64) FILENAME="v2ray-linux-arm64-v8a.zip";;
