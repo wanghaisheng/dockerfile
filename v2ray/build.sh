d1badda167c1c7c1419d6b94a93cf3b5aa0924d6
Tarocch1 <huiben.fang@gmail.com>
2021-01-06 09:59:55 +0800

    v2ray 去掉 riscv64

--- v2ray/build.sh
+++ v2ray/build.sh
@@ -3,7 +3,7 @@ cd ./v2ray
 read VERSION < ./version
 
 docker buildx build \
-  --platform linux/amd64,linux/386,linux/riscv64,linux/arm64,linux/arm/v7,linux/arm/v6 \
+  --platform linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6 \
   -t tarocch1/v2ray:latest \
   -t tarocch1/v2ray:${VERSION} \
   -t registry.cn-hongkong.aliyuncs.com/tarocch1/v2ray:latest \
