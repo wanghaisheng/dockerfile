d974227fe429eabd049d9bec130f0266951dbe57
Tarocch1 <huiben.fang@gmail.com>
2021-03-11 11:39:45 +0800

    ci(xray): 新增 xray

---
+++ xray/build.sh
@@ -0,0 +1,11 @@
+cd ./xray
+
+read VERSION < ./version
+
+docker buildx build \
+  --platform linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/riscv64,linux/ppc64le,linux/s390x,linux/mips64,linux/mips64le \
+  -t tarocch1/xray:latest \
+  -t tarocch1/xray:${VERSION} \
+  -t registry.cn-hongkong.aliyuncs.com/tarocch1/xray:latest \
+  --push \
+  .
