3980da05f747c76b1e090479b362f685f3051a03
Tarocch1 <huiben.fang@gmail.com>
2022-03-13 00:08:13 +0800

    chore: remove adminer

--- adminer/build.sh
+++
@@ -1,7 +0,0 @@
-cd ./adminer
-
-docker buildx build \
-  --platform linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
-  -t registry.cn-hongkong.aliyuncs.com/tarocch1/adminer:latest \
-  --push \
-  .
